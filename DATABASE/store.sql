ff3c848723eaa9c97ae7805e8084650bd59ff217
Mario Bojarovski <bojarovskimario@gmail.com>
2022-01-24 18:33:31 +0100

    DataBase

--- DATABASE/store.sql
+++ DATABASE/store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 22, 2022 at 09:07 PM
+-- Generation Time: Jan 24, 2022 at 06:32 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.14
 
@@ -37,7 +37,9 @@ CREATE TABLE `brand` (
 --
 
 INSERT INTO `brand` (`BrandId`, `brandName`) VALUES
-(1, 'Nike');
+(1, 'Nike'),
+(2, 'Addidas'),
+(7, 'Gucci');
 
 -- --------------------------------------------------------
 
@@ -56,6 +58,28 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`CategoryId`, `categoryName`) VALUES
 (1, 'Casual');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `finalorder`
+--
+
+CREATE TABLE `finalorder` (
+  `finalOrderID` int(11) NOT NULL,
+  `orderinfoId` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `snakersId` int(11) NOT NULL,
+  `status` varchar(30) NOT NULL DEFAULT 'To Be Shipped'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `finalorder`
+--
+
+INSERT INTO `finalorder` (`finalOrderID`, `orderinfoId`, `username`, `date`, `snakersId`, `status`) VALUES
+(17, 2, 'admin', '2022-01-16', 3, 'Shepped');
 
 -- --------------------------------------------------------
 
@@ -99,16 +123,6 @@ CREATE TABLE `orders` (
   `Status` varchar(20) NOT NULL DEFAULT 'To Be Shipped'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`orderId`, `orderinfoId`, `username`, `date`, `SneakersId`, `Status`) VALUES
-(162, 32, 'admin', '2022-01-22', 3, 'To Be Shipped'),
-(163, 32, 'admin', '2022-01-22', 3, 'To Be Shipped'),
-(164, 32, 'admin', '2022-01-22', 3, 'To Be Shipped'),
-(165, 32, 'admin', '2022-01-22', 3, 'To Be Shipped');
-
 -- --------------------------------------------------------
 
 --
@@ -128,15 +142,16 @@ CREATE TABLE `ordersinfo` (
   `Phone` int(11) NOT NULL,
   `Zip` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `message` text NOT NULL
+  `message` text NOT NULL,
+  `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `ordersinfo`
 --
 
-INSERT INTO `ordersinfo` (`ordersinfoId`, `informationId`, `UserId`, `FirstName`, `LastName`, `Country`, `Addres1`, `Addres2`, `City`, `Phone`, `Zip`, `email`, `message`) VALUES
-(32, 1, 'admin', 'mmmmmm', 'fcvctghvyu       ', 'Albania', 'Neue Amberger Straße', '44', 'Grafenwöhr', 2147483647, 92655, 'mariobojarovski1407@gmail.com', '');
+INSERT INTO `ordersinfo` (`ordersinfoId`, `informationId`, `UserId`, `FirstName`, `LastName`, `Country`, `Addres1`, `Addres2`, `City`, `Phone`, `Zip`, `email`, `message`, `price`) VALUES
+(58, 1, 'admin', 'mmmmmm', 'fcvctghvyu       ', 'Albania', 'Neue Amberger Straße', '44', 'Grafenwöhr', 2147483647, 92655, 'mariobojarovski1407@gmail.com', '', 211);
 
 -- --------------------------------------------------------
 
@@ -163,7 +178,9 @@ CREATE TABLE `sneakers` (
 
 INSERT INTO `sneakers` (`SneakersId`, `Name`, `Price`, `Description`, `Quantity`, `Photo`, `Photo2`, `Photo3`, `CategoryId`, `BrandId`) VALUES
 (1, 'Air Force', 101.99, '\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque rhoncus ultrices sem sit amet tincidunt. Nam ultricies eu urna quis varius. Suspendisse arcu urna, gravida in laoreet in, ullamcorper et arcu. Suspendisse lectus tortor, mollis eu odio sit amet, fermentum porttitor eros. Vivamus blandit pulvinar mauris. Sed dignissim, nisl vel tristique scelerisque, urna dolor egestas lectus, non imperdiet mi est at dolor. Nunc sed ligula eu lacus maximus varius. Morbi fermentum finibus neque id egestas. Curabitur posuere tristique condimentum. Ut ac erat vel nibh ornare pharetra sed id nibh. Fusce ac tempus dolor. In quis dapibus turpis. Vivamus posuere nunc et justo ornare, a tincidunt ex porta. Sed dapibus, augue et aliquet vestibulum, erat ante ullamcorper sapien, eget eleifend massa eros non ante. Fusce nec ex id dui dapibus dignissim eu posuere sem.', 10, 'img/product/air-force-1.png', 'img/product/air-force-1.2.png', 'img/product/air-force-1.3.png', 1, 1),
-(3, 'Air Max 97', 175, 'dadasdasdasd', 100, 'img/product/air-max-97.png', 'img/product/air-max-97.1.png', 'img/product/air-max-97.2.png', 1, 1);
+(3, 'Air Max 97', 175, 'dadasdasdasd', 100, 'img/product/air-max-97.png', 'img/product/air-max-97.1.png', 'img/product/air-max-97.2.png', 1, 1),
+(4, 'ULTRABOOST 22 SHOES', 179, 'Running takes energy. These adidas shoes give it back. BOOST in the midsole harnesses and returns the force of every footstrike while a Linear Energy Push system guides the heel, propelling you toward your next step. The adidas PRIMEKNIT+ upper adapts to the changing shape of your foot as you go.\r\n\r\nThis shoe\'s upper is made with a high-performance yarn which contains at least 50% Parley Ocean Plastic —  reimagined plastic waste, intercepted on remote islands, beaches, coastal communities and shorelines, preventing it from polluting our ocean. The other 50% of the yarn is recycled polyester.', 10, 'img/product/Ultraboost_22_ccexpress.png', 'img/product/Ultraboost_22.1_ccexpress.png', 'img/product/Ultraboost_22.2_ccexpress.png', 1, 2),
+(6, 'Nike Air Max 95', 231, 'sadsad', 32, '', '', '', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -216,6 +233,12 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`CategoryId`);
 
 --
+-- Indexes for table `finalorder`
+--
+ALTER TABLE `finalorder`
+  ADD PRIMARY KEY (`finalOrderID`);
+
+--
 -- Indexes for table `informations`
 --
 ALTER TABLE `informations`
@@ -259,7 +282,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `brand`
 --
 ALTER TABLE `brand`
-  MODIFY `BrandId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `BrandId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `finalorder`
+--
+ALTER TABLE `finalorder`
+  MODIFY `finalOrderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `informations`
@@ -271,25 +300,25 @@ ALTER TABLE `informations`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=169;
+  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=218;
 
 --
 -- AUTO_INCREMENT for table `ordersinfo`
 --
 ALTER TABLE `ordersinfo`
-  MODIFY `ordersinfoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `ordersinfoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT for table `sneakers`
 --
 ALTER TABLE `sneakers`
-  MODIFY `SneakersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `SneakersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `temporder`
 --
 ALTER TABLE `temporder`
-  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT for table `users`
